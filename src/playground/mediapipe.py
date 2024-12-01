a624d7c11a809cfd5dd676ca6646dfd222f92fb9
Sean D Kim <seandkim14@gmail.com>
2024-11-30 17:34:03 -0800

    nit mediapipe folder rename

--- src/playground/mediapipe.py
+++ src/playground/mediapipe.py
@@ -8,12 +8,13 @@ mp_drawing_styles = mp.solutions.drawing_styles
 mp_hands = mp.solutions.hands
 
 # For static images:
-OUTPUT_DIR = ["./images/processed"]
+INPUT_DIR = "./images/raw"
+OUTPUT_DIR = "./images/processed/mediapipe"
 IMAGE_FILES = [
   # "./images/raw/hand.png", 
   # "./images/raw/guitar.png", # doesn't work
   # "./images/raw/guitar2.png" # doesn't work
-  "./images/raw/guitar3.png"
+  "guitar3.png"
 ]
 
 with mp_hands.Hands(
@@ -23,7 +24,7 @@ with mp_hands.Hands(
   for idx, file in enumerate(IMAGE_FILES):
     # Read an image, flip it around y-axis for correct handedness output (see
     # above).
-    image = cv2.flip(cv2.imread(file), 1)
+    image = cv2.flip(cv2.imread(f"{INPUT_DIR}/{file}"), 1)
     # Convert the BGR image to RGB before processing.
     results = hands.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
@@ -47,7 +48,7 @@ with mp_hands.Hands(
           mp_drawing_styles.get_default_hand_landmarks_style(),
           mp_drawing_styles.get_default_hand_connections_style())
     cv2.imwrite(
-        'OUTPUT_DIR' + str(idx) + '.png', cv2.flip(annotated_image, 1))
+        f"{OUTPUT_DIR}/{file}", cv2.flip(annotated_image, 1))
     # Draw hand world landmarks.
     if not results.multi_hand_world_landmarks:
       continue
